290a0f27bdf093e8e0eccf8a0485de41099882b8
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-06-14 21:01:24 -0500

    add repeatString.js repeatString.spec.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    if (num < 0) {
+        result = "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        result = result + string;
+    }
+    return result;
 };
+
+let number = Math.floor(Math.random() * 1000);
+
+console.log(repeatString("", 10));
+
 
 // Do not edit below this line
 module.exports = repeatString;
