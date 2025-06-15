d42db6e5fd20457b1bc078b79f1c2b5e27a2b6c9
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-06-15 12:11:07 -0500

    add reverserString.js reverseString.spec.js

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    
+    for (let i = 0; i < string.length; i++) {
+        reverse += string.slice(string.length - (i + 1), string.length - i);
+    }
+    // console.log(string.length);
+    return reverse;
 };
+
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
