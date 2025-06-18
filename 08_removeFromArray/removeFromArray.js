e7edecfef873e1aec9e5bd39db9050d23d74544f
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-06-18 14:25:36 -0500

    add removeFromArray.js removeFrom.spec.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+
+const removeFromArray = function(array, ...num) {
+    
+    return array.filter(element => !num.includes(element));
+    
+}
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
