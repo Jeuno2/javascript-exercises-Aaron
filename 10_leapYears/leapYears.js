d1d54cefafaf39bb0e90173bc532465c940349e5
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-06-19 13:42:05 -0500

    add leapYears.js leapYears.spec.js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // test conditions
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    }
+    else if (year % 4 !== 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
 };
+
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
