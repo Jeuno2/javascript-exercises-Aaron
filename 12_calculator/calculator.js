1371df17ecc9804eac9ced4376ade4db9a1f684f
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-06-19 15:56:38 -0500

    add calculator.js calculator.spec.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for(let x in array) {
+    sum += array[x];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = array[0];
+  for (let i = 1; i < array.length; i++) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0 || x === 1) {
+    return 1;
+  }
+  return x * factorial(x - 1);
 };
+
+console.log(add(2, 6));
+console.log(subtract(-8, 7));
+console.log(sum([1, 3, 5, 7, 9]));
+console.log(multiply([2, 4, 6, 8, 10, 12, 14]));
+console.log(power(2, 6));
+console.log(factorial(5));
+
 
 // Do not edit below this line
 module.exports = {
