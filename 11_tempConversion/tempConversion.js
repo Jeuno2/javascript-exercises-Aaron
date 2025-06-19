ecbc8413996538e104ad5ca58ae3df14ee17a463
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-06-19 14:39:55 -0500

    add tempConversion.js tempConversion.spec.js

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  return Number(((far - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return Number((((cel * (9 / 5)) + 32)).toFixed(1));
 };
+
+console.log(`${convertToCelsius(-100)} is the temp in Celcius`);
+console.log(`${convertToFahrenheit(-10)} is the temp in Fahrenheit`);
 
 // Do not edit below this line
 module.exports = {
