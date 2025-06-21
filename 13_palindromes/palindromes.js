46c4023b894aebcccbd8a0f1e145cca525d7fa98
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-06-21 13:05:38 -0500

    add palindromes.js palindromes.spec.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    let reverse = "";
+
+    let cleanedString = word.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    
+    console.log(cleanedString);
+
+    for (let i = 0; i < cleanedString.length; i++) {
+        reverse += cleanedString.slice(cleanedString.length - (i + 1), cleanedString.length - i);
+    }
+
+    if (cleanedString === reverse) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+    console.log(reverse);
 };
+
+console.log(palindromes("Racecar!"));
 
 // Do not edit below this line
 module.exports = palindromes;
