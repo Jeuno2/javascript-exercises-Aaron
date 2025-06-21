cb1d52a00f03cfa87f387318c5f8f6f124bdb1c4
Jeuno2 <Jeuno2@users.noreply.github.com>
2025-06-21 16:16:08 -0500

    add fibonacci.js fibonacci.spec.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    member = Number(member);
+    if (member < 0) {
+        return "OOPS";
+    }
+    if (member === 0) {
+        return 0;
+    }
+    let num1 = 1;
+    let num2 = 1;
+    let sum = 0;
 
-};
+    for (let i = 1; i < member; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num1
+}
+
+console.log(fibonacci("8"));
 
 // Do not edit below this line
 module.exports = fibonacci;
